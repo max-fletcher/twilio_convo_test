dc416470c8313d654d935a6783cefefda48179d6
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2024-11-12 10:06:32 +0600

    Added functions for sending message as admin

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,21 @@ function App() {
   const [messages, setMessages] = useState([])
   const [user, setUser] = useState()
 
-  const appUser = 'usr_56913465891350'
-  const professional = 'pro_hn9a8wdh89ahd'
+  const appUserId = 'usr_56913465891350'
+  const professionalId = 'pro_hn9a8wdh89ahd'
+  const appUserAuthToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InVzcl81NjkxMzQ2NTg5MTM0MCIsIm5hbWUiOiJBZGFtIFNtaXRoMiIsInVzZXJuYW1lIjoiamhuc210aGJvaXMyIiwiZW1haWwiOiJtYWhpbi5jaG93ZGh1cnkuMTk5MUBnbWFpbC5jb20iLCJwaG9uZSI6Iis4ODAxNzYyMjE0MzE1Iiwid2hhdHNhcHBfbm8iOm51bGwsInZlcmlmaWVkIjp0cnVlLCJndWVzdCI6dHJ1ZSwiaWF0IjoxNzMxMjMxMjU3LCJleHAiOjE3MzM4MjMyNTd9.f9i82nigIRo1t3oBtdQSdQI-rgxtxWCcmCgAAKTDAYg"
+  const proAuthToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InVzcl81NjkxMzQ2NTg5MTM0MCIsIm5hbWUiOiJBZGFtIFNtaXRoMiIsInVzZXJuYW1lIjoiamhuc210aGJvaXMyIiwiZW1haWwiOiJtYWhpbi5jaG93ZGh1cnkuMTk5MUBnbWFpbC5jb20iLCJwaG9uZSI6Iis4ODAxNzYyMjE0MzE1Iiwid2hhdHNhcHBfbm8iOm51bGwsInZlcmlmaWVkIjp0cnVlLCJndWVzdCI6dHJ1ZSwiaWF0IjoxNzMxMjMxMjU3LCJleHAiOjE3MzM4MjMyNTd9.f9i82nigIRo1t3oBtdQSdQI-rgxtxWCcmCgAAKTDAYg"
+  const connectionUniqueId = 'con_0B4PIHjbYn'
   const pusherClient = new Pusher(import.meta.env.VITE_PUSHER_APP_KEY, {
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     channelAuthorization: {
-      endpoint: import.meta.env.VITE_PUSHER_AUTH_BASE_URL + `${appUser}/${professional}`
+      endpoint: import.meta.env.VITE_PUSHER_AUTH_BASE_URL + `${appUserId}/${professionalId}`
     }
   });
 
-  const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InVzcl81NjkxMzQ2NTg5MTM0MCIsIm5hbWUiOiJBZGFtIFNtaXRoMiIsInVzZXJuYW1lIjoiamhuc210aGJvaXMyIiwiZW1haWwiOiJtYWhpbi5jaG93ZGh1cnkuMTk5MUBnbWFpbC5jb20iLCJwaG9uZSI6Iis4ODAxNzYyMjE0MzE1Iiwid2hhdHNhcHBfbm8iOm51bGwsInZlcmlmaWVkIjp0cnVlLCJndWVzdCI6dHJ1ZSwiaWF0IjoxNzMxMjMxMjU3LCJleHAiOjE3MzM4MjMyNTd9.f9i82nigIRo1t3oBtdQSdQI-rgxtxWCcmCgAAKTDAYg"
-  const connectionUniqueId = 'con_0B4PIHjbYn'
-
   useEffect(() => {
     console.log('henlo');
-    getMessages()
+    getMessagesForAppUser()
     console.log(pusherClient);
     pusherBindEvents()
     return (() => {
@@ -29,18 +29,18 @@ function App() {
     })
   }, [])
 
-  const getMessages = async () => {
-    const response = await fetch(import.meta.env.VITE_PUSHER_BASE_URL + `/professionals/get_connection_chat_messages/pro_hn9a8wdh89ahd`, {
+  const getMessagesForAppUser = async () => {
+    const response = await fetch(import.meta.env.VITE_PUSHER_BASE_URL + `/professionals/get_connection_chat_messages_as_user/pro_hn9a8wdh89ahd`, {
         headers: {
           "Content-Type": "application/json",
-          "Authorization": `Bearer ${authToken}`
+          "Authorization": `Bearer ${appUserAuthToken}`
         },
       }
     )
     const data = await response.json()
     setMessages(data.data.chat_messages)
 
-    console.log('data', data, 'messages', messages);
+    console.log('appUser msg data', data, 'app_User messages', messages);
   }
 
   const pusherBindEvents = async () => {
@@ -62,12 +62,26 @@ function App() {
     });
   };
 
-  const sendMessage = async () => {
-    await fetch(import.meta.env.VITE_PUSHER_BASE_URL + `/professionals/send_message_to_professional/pro_hn9a8wdh89ahd`, {
+  const sendMessageToPro = async () => {
+    await fetch(import.meta.env.VITE_PUSHER_BASE_URL + `/professionals/send_message_to_professional/` + professionalId, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${authToken}`
+        Authorization: `Bearer ${appUserAuthToken}`
+      },
+      body:JSON.stringify({ message: messageToBeSent })
+    })
+
+    setMessageToBeSent('')
+    console.log('sent');
+  }
+
+  const sendMessageToAppUser = async () => {
+    await fetch(import.meta.env.VITE_PUSHER_BASE_URL + `/professionals/send_message_to_app_user/` + appUserId, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${appUserAuthToken}`
       },
       body:JSON.stringify({ message: messageToBeSent })
     })
@@ -88,7 +102,8 @@ function App() {
       <br />
       <input type="text" onChange={(e) => setMessageToBeSent(e.target.value)} value={messageToBeSent} />
       <br />
-      <button type="button" onClick={sendMessage}>Click Me!</button>
+      <button type="button" onClick={sendMessageToPro}>Send Message To Pro</button>
+      <button type="button" onClick={sendMessageToAppUser}>Send Message To User</button>
     </>
   )
 }
