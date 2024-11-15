9702bbedbd82ad58ccf40f272d52ef3b12b05fa7
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2024-11-16 00:43:08 +0600

    Done. App2 contains group-chat implementation.

--- src/App2.jsx
+++ src/App2.jsx
@@ -2,23 +2,21 @@ import { useEffect, useState } from 'react'
 import Pusher from 'pusher-js';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState({})
   const [messageToBeSent, setMessageToBeSent] = useState('')
   const [allMessages, setAllMessages] = useState([])
-  const [membersList, setMembersList] = useState([])
   const [membersCount, setMembersCount] = useState(0)
-  const [joinedMembersList, setJoinedMembersList] = useState([])
   const [joinedMembersCount, setJoinedMembersCount] = useState(0)
   const [statusMessage, setStatusMessage] = useState(null)
 
   const appUserId = 'usr_56913465891340'
-  const appUserId2 = 'pro_hn9a8wdh89ahd'
-  const appUserAuthToken1 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InVzcl81NjkxMzQ2NTg5MTM0MCIsIm5hbWUiOiJBZGFtIFNtaXRoMiIsInVzZXJuYW1lIjoiamhuc210aGJvaXMyIiwiZW1haWwiOiJtYWhpbi5jaG93ZGh1cnkuMTk5MUBnbWFpbC5jb20iLCJwaG9uZSI6Iis4ODAxNzYyMjE0MzE1Iiwid2hhdHNhcHBfbm8iOm51bGwsInZlcmlmaWVkIjp0cnVlLCJndWVzdCI6dHJ1ZSwiaWF0IjoxNzMxMzAwNTA5LCJleHAiOjE3MzM4OTI1MDl9.8LJZhK5SlAj4MueEQzLl0WzyWwQHqQy4Wa_DIQMEofI"
-  const appUserAuthToken2 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2YWx1ZSI6InBybzFAbWFpbC5jb20iLCJpZCI6ImFkbV9iaDdhc2RnNzg5YSIsInVzZXJUeXBlIjoiUFJPRkVTU0lPTkFMIiwiaWF0IjoxNzMxMzg2NTMxLCJleHAiOjE3MzM5Nzg1MzF9.PECQijHEfxXE1In0kdFDB8xPHmXF8rqz1hMkW1PlAto"
-  const auctionUniqueId = 'auc_9ht13rj0ZX'
-  const batchUniqueId = 'batch_og53f2AkcO'
+  const appUserId2 = 'usr_56913465891350'
+  const appUserAuthToken1 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InVzcl81NjkxMzQ2NTg5MTM0MCIsIm5hbWUiOm51bGwsInVzZXJuYW1lIjoiZmxldGNoZXIiLCJlbWFpbCI6Im1haGluLmNob3dkaHVyeS4xOTkxQGdtYWlsLmNvbSIsInBob25lIjoiKzg4MDE3NjIyMTQzMTUiLCJ3aGF0c2FwcF9ubyI6bnVsbCwidmVyaWZpZWQiOnRydWUsImd1ZXN0Ijp0cnVlLCJpYXQiOjE3MzE2NzE3NjcsImV4cCI6MTczNDI2Mzc2N30.Mzd1_7h2Zs7GoLDbp-L_CihzPqnUQw-NcUgHPVk31yM"
+  const appUserAuthToken2 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InVzcl81NjkxMzQ2NTg5MTM1MCIsIm5hbWUiOm51bGwsInVzZXJuYW1lIjoiVXNlciAyIiwiZW1haWwiOiJ1c2VyMkBtYWlsLmNvbSIsInBob25lIjoiMjM0NTY3ODkiLCJ3aGF0c2FwcF9ubyI6bnVsbCwidmVyaWZpZWQiOnRydWUsImd1ZXN0IjpmYWxzZSwiaWF0IjoxNzMxNjgwNjkzLCJleHAiOjE3MzQyNzI2OTN9.nfJHk36vdGZOB7p5A_avgn_RXzA7Nr-J582l1idBbu4"
+  const auctionUniqueId = 'auc_9bZJfSrzRy'
+  const batchUniqueId = 'batch_SULmkKLk6g'
   const urlParams = new URLSearchParams(window.location.search);
   const userNo = urlParams.get('userNo'); // 'FIRST'|'SECOND'
-  console.log(userNo);
   const pusherClient = new Pusher(import.meta.env.VITE_PUSHER_APP_KEY, {
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     channelAuthorization: {
@@ -38,11 +36,14 @@ function App() {
   }, [])
 
   const pusherBindEvents = async () => {
-    console.log('bound');
     var channel = pusherClient.subscribe(`presence-auction-live-chat-${auctionUniqueId}-batch-${batchUniqueId}`);
 
     channel.bind("pusher:subscription_succeeded", (members) => {
-      console.log('all members details', members.count, members);
+      
+      var thisUser = channel.members.me;
+      setCurrentUser({ id: thisUser.id, ...thisUser.info })
+
+      // console.log('all members details', members.count, members);
       setJoinedMembersCount(members.count)
       // LIST OF JOINED MEMBERS
       members.each((member) => {
@@ -51,11 +52,9 @@ function App() {
     });
     channel.bind("pusher:member_added", (member) => {
       setStatusMessage(`${member.info.username} has joined the chat`)
-      setJoinedMembersList(prevMembers => [data, ...prevMembers])
     });
     channel.bind("pusher:member_removed", (member) => {
       setStatusMessage(`${member.info.username} has left the chat`)
-      setJoinedMembersList(prevMembers => prevMembers.filter(prevMember => prevMember.id !== member.id))
     });
     channel.bind("pusher:subscription_error", (data) => {
         console.log('subscription_error', data)
@@ -74,9 +73,7 @@ function App() {
       }
     )
     const data = await response.json()
-    console.log(data)
-    setMembersList(data.data.joined_members)
-    setMembersCount(data.data.joined_members.length)
+    setMembersCount(data.data.joined_users_count)
   }
 
   const getAllMessages = async () => {
@@ -89,13 +86,13 @@ function App() {
       }
     )
     const data = await response.json()
-    console.log(data.data.group_chat_messages)
+    console.log('all group chat messages', data.data.group_chat_messages)
     setAllMessages(data.data.group_chat_messages)
   }
 
   // FOR APP USERS
   const sendMessage = async () => {
-    const response = await fetch(import.meta.env.VITE_PUSHER_BASE_URL2 + `/auctions/app/get_auction_batch_group_chat_messages/${auctionUniqueId}/batch/${batchUniqueId}`, {
+    const response = await fetch(import.meta.env.VITE_PUSHER_BASE_URL2 + `/auctions/app/send_message_to_group_chat/${auctionUniqueId}/batch/${batchUniqueId}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -112,35 +109,16 @@ function App() {
       <h5>{statusMessage ? statusMessage : ''}</h5>
       <p>{joinedMembersCount}/{membersCount}</p>
       <br />
-      {/* <p>
-        All Members List: {
-          membersList.map((member, index) => {
-            return <div key={index}>
-              <p>id: {member.id}</p>
-              <p>Name: {member.name}</p>
-              <p>Username: {member.username}</p>
-              <hr />
-            </div>
-          })
-        }
+      <p>
+        CurrentUser: 
+        Id: { currentUser.id }
+        Name: { currentUser.name }
+        Username: { currentUser.username }
       </p>
       <br />
-      <p>
-        Joined Members List: {
-          joinedMembersList.map((joinedMember, index) => {
-            return <div key={index}>
-              <p>id: {joinedMember.id}</p>
-              <p>Name: {joinedMember.name}</p>
-              <p>Username: {joinedMember.username}</p>
-              <hr />
-            </div>
-          })
-        }
-      </p> */}
-      <br />
-      <div style={{ display: "flex" }}>
+      <div>
         <br />
-        {/* FOR APP USERS */}
+        {/* FOR APP USER 1 */}
         <div style={{ padding: "25px", borderRightStyle: 'solid', borderRightColor: 'white', borderRightWidth: 2 }}>
           <h6> All Messages: </h6>
           {allMessages.map((msg, index) => {
@@ -154,7 +132,7 @@ function App() {
       <input type="text" onChange={(e) => setMessageToBeSent(e.target.value)} value={messageToBeSent} />
       <br />
       <br />
-      <button type="button" onClick={sendMessage}>Send Message To Pro</button>
+      <button type="button" onClick={sendMessage}>Send Message</button>
     </>
   )
 }
