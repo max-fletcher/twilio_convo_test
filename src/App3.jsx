9447f8d9bb55a71c465575d0b8a2d552e2469ce2
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2024-11-18 11:58:10 +0600

    Before user-specific tests

--- src/App3.jsx
+++ src/App3.jsx
@@ -5,19 +5,17 @@ function App() {
   const [messageToBeSent, setMessageToBeSent] = useState('')
   const [appUserMessages, setAppUserMessages] = useState([])
   const [proMessages, setProMessages] = useState([])
-  const [proIsOnline, setProIsOnline] = useState(false)
-  const [userIsOnline, setUserIsOnline] = useState(false)
   const [statusMessage, setStatusMessage] = useState(null)
-  const [proChannel, setProChannel]=useState(null)
-  const [userChannel, setUserChannel]=useState(null)
+  const [proChannel, setProChannel] = useState(null)
+  const [userChannel, setUserChannel] = useState(null)
 
   const appUserId = 'usr_56913465891340'
-  const professionalId = 'pro_jadjha98w'
+  const professionalId = 'pro_hn9a8wdh89ahd'
   // const usertype = 'APPUSER' // 'APPUSER'|'PROFESSIONAL'
   const urlParams = new URLSearchParams(window.location.search);
   const usertype = urlParams.get('usertype'); // 'APPUSER'|'PROFESSIONAL'
-  const appUserAuthToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InVzcl81NjkxMzQ2NTg5MTM0MCIsIm5hbWUiOm51bGwsInVzZXJuYW1lIjoiZmxldGNoZXIiLCJlbWFpbCI6Im1haGluLmNob3dkaHVyeS4xOTkxQGdtYWlsLmNvbSIsInBob25lIjoiKzg4MDE3NjIyMTQzMTUiLCJ3aGF0c2FwcF9ubyI6bnVsbCwidmVyaWZpZWQiOnRydWUsImd1ZXN0Ijp0cnVlLCJpYXQiOjE3MzE4NjAzOTksImV4cCI6MTczNDQ1MjM5OX0.fnfu3E3g5W2Zdyj4ge1N7Vo0E9DqE7Wvf8vEVapVGWI"
-  const proAuthToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2YWx1ZSI6InBybzFAZXhhbXBsZS5jb20iLCJpZCI6IjMiLCJ1c2VyVHlwZSI6IlBST0ZFU1NJT05BTCIsImlhdCI6MTczMTg2MDM4NCwiZXhwIjoxNzM0NDUyMzg0fQ.697R3Ia1_wjmLa8LSIDIj7agYz-gsrpC3qP6JnXO9A4"
+  const appUserAuthToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InVzcl81NjkxMzQ2NTg5MTM0MCIsIm5hbWUiOiJBZGFtIFNtaXRoMiIsInVzZXJuYW1lIjoiamhuc210aGJvaXMyIiwiZW1haWwiOiJtYWhpbi5jaG93ZGh1cnkuMTk5MUBnbWFpbC5jb20iLCJwaG9uZSI6Iis4ODAxNzYyMjE0MzE1Iiwid2hhdHNhcHBfbm8iOm51bGwsInZlcmlmaWVkIjp0cnVlLCJndWVzdCI6dHJ1ZSwiaWF0IjoxNzMxOTA1ODk0LCJleHAiOjE3MzQ0OTc4OTR9.K_yKCU21ILKjOvRVu8uB2GWaMAZY3dIPrIDCUPcVTZ8"
+  const proAuthToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2YWx1ZSI6InBybzFAbWFpbC5jb20iLCJpZCI6ImFkbV9iaDdhc2RnNzg5YSIsInVzZXJUeXBlIjoiUFJPRkVTU0lPTkFMIiwiaWF0IjoxNzMxOTA1OTA1LCJleHAiOjE3MzQ0OTc5MDV9.XDLh0g_wJm5CAEBgdY_kdRw1HfqjpkHbyiomoWGPgug"
 
   useEffect(() => {
     const pusherClient = new Pusher(import.meta.env.VITE_PUSHER_APP_KEY, {
@@ -29,11 +27,9 @@ function App() {
 
     const pusherBindEvents = async () => {
       // FOR APP USER
-      const userChannel = pusherClient.subscribe(`presence-for-pro-chat-channel-${professionalId}`);
+      const userChannel = pusherClient.subscribe(`presence-user-chat-listener-${appUserId}`);
       userChannel.bind("pusher:subscription_succeeded", (members) => {
-        console.log('pro channel all members details', members.count, members);
-        if(members.count === 2)
-          setProIsOnline(true)
+        console.log('user channel all members details', members.count, members);
         // LIST OF JOINED MEMBERS
         members.each((member) => {
           console.log('member', member)
@@ -41,11 +37,9 @@ function App() {
       });
       userChannel.bind("pusher:member_added", (member) => {
         setStatusMessage(`${member.info.username} has joined the chat`)
-        setProIsOnline(true)
       });
       userChannel.bind("pusher:member_removed", (member) => {
         setStatusMessage(`${member.info.username} has left the chat`)
-        setProIsOnline(false)
       });
       userChannel.bind("pusher:subscription_error", (data) => {
         console.log('subscription_error', data)
@@ -53,19 +47,18 @@ function App() {
       userChannel.bind("pusher:subscription_error", (data) => {
         console.log('subscription_error', data)
       });
-      userChannel.bind(`client-send-message-user-chat-${appUserId}`, (data) => {
+      userChannel.bind(`send-message-to-user-chat`, (data) => {
+        console.log('send-message-to-user-chat');
         setAppUserMessages(prevMessages => [...prevMessages, data]) // FOR APP USERS
         setProMessages(prevMessages => [...prevMessages, data]) // FOR PROS
       });
       setUserChannel(userChannel)
-      getMessagesForAppUser()
+      getMessagesForPro()
 
       // FOR PRO
-      const proChannel = pusherClient.subscribe(`presence-for-user-chat-channel-${appUserId}`);
+      const proChannel = pusherClient.subscribe(`presence-pro-chat-listener-${professionalId}`);
       proChannel.bind("pusher:subscription_succeeded", (members) => {
-        console.log('user channel all members details', members.count, members);
-        if(members.count === 2)
-          setUserIsOnline(true)
+        console.log('pro channel all members details', members.count, members);
         // LIST OF JOINED MEMBERS
         members.each((member) => {
           console.log('member', member)
@@ -73,11 +66,9 @@ function App() {
       });
       proChannel.bind("pusher:member_added", (member) => {
         setStatusMessage(`${member.info.username} has joined the chat`)
-        setUserIsOnline(true)
       });
       proChannel.bind("pusher:member_removed", (member) => {
         setStatusMessage(`${member.info.username} has left the chat`)
-        setUserIsOnline(false)
       });
       proChannel.bind("pusher:subscription_error", (data) => {
         console.log('subscription_error', data)
@@ -85,20 +76,21 @@ function App() {
       proChannel.bind("pusher:subscription_error", (data) => {
         console.log('subscription_error', data)
       });
-      proChannel.bind(`client-send-message-pro-chat-${professionalId}`, (data) => {
+      proChannel.bind(`send-message-to-pro-chat`, (data) => {
+        console.log('send-message-to-pro-chat');
         setAppUserMessages(prevMessages => [...prevMessages, data]) // FOR APP USERS
         setProMessages(prevMessages => [...prevMessages, data]) // FOR PROS
       });
       setProChannel(proChannel)
-      getMessagesForPro()
+      getMessagesForAppUser()
     };
 
     console.log('bound');
     console.log(pusherClient)
     pusherBindEvents()
     return (() => {
-      pusherClient.unsubscribe(`presence-for-pro-chat-channel-${professionalId}`)
-      pusherClient.unsubscribe(`presence-for-user-chat-channel-${appUserId}`)
+      pusherClient.unsubscribe(`presence-pro-chat-listener-${professionalId}`)
+      pusherClient.unsubscribe(`presence-user-chat-listener-${appUserId}`)
     })
   }, [])
 
@@ -116,9 +108,16 @@ function App() {
   }
 
   const sendMessageToPro = async () => {
-    userChannel.trigger(`client-send-message-pro-chat-${professionalId}`, { sent_by_user: true, professional_id: professionalId, user_id: appUserId, message: messageToBeSent })
+    await fetch(import.meta.env.VITE_PUSHER_BASE_URL + `/app/professionals/send_message_to_professional/v2/` + professionalId, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${appUserAuthToken}`
+      },
+      body:JSON.stringify({ message: messageToBeSent })
+    })
     setMessageToBeSent('')
-    console.log('sent')
+    console.log('sent');
   }
 
   // FOR PROS
@@ -135,16 +134,21 @@ function App() {
   }
 
   const sendMessageToAppUser = async () => {
-    proChannel.trigger(`client-send-message-user-chat-${appUserId}`, { sent_by_user: false, professional_id: professionalId, user_id: appUserId, message: messageToBeSent })
+    await fetch(import.meta.env.VITE_PUSHER_BASE_URL3 + `/professionals/send_message_to_app_user/v2/` + appUserId, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${proAuthToken}`
+      },
+      body:JSON.stringify({ message: messageToBeSent })
+    })
     setMessageToBeSent('')
-    console.log('sent')
+    console.log('sent');
   }
 
   return (
     <>
       <h5>{statusMessage ? statusMessage : ''}</h5>
-      <h5>{proIsOnline ? 'Professional is online' : 'Professional is offline'}</h5>
-      <h5>{userIsOnline ? 'APp user is online' : 'APp user is offline'}</h5>
       <div style={{ display: "flex" }}>
         <br />
         {/* FOR APP USERS */}
