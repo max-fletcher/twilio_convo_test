320763c66542638e9403c480e38666cfe4a490ed
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2024-11-21 16:24:03 +0600

    auction event placeBid done.

--- src/App4.jsx
+++ src/App4.jsx
@@ -6,21 +6,22 @@ function App() {
   const [onlineCount, setOnlineCount] = useState(0)
   const [statusMessage, setStatusMessage] = useState(null)
 
-  const auctionId = ""
-  const batchId = ""
-  const userId = ""
+  const auctionUniqueId = "auc_qr5IhJktQP"
+  const batchUniqueId = "batch_CoGdtOO2Uv"
+  const appUserId = "usr_56913465891340"
+  const appUserToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InVzcl81NjkxMzQ2NTg5MTM0MCIsIm5hbWUiOiJBZGFtIFNtaXRoMiIsInVzZXJuYW1lIjoiamhuc210aGJvaXMyIiwiZW1haWwiOiJtYWhpbi5jaG93ZGh1cnkuMTk5MUBnbWFpbC5jb20iLCJwaG9uZSI6Iis4ODAxNzYyMjE0MzE1Iiwid2hhdHNhcHBfbm8iOm51bGwsInZlcmlmaWVkIjp0cnVlLCJndWVzdCI6dHJ1ZSwiaWF0IjoxNzMyMTYzOTg0LCJleHAiOjE3MzQ3NTU5ODR9.GMKWuhspnhJsP4jl4KfAZu6gfmivB8eHZ3FxsTYwIGs"
 
   useEffect(() => {
     const pusherClient = new Pusher(import.meta.env.VITE_PUSHER_APP_KEY, {
       cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
       channelAuthorization: {
-        endpoint: import.meta.env.VITE_PUSHER_AUTH_BASE_URL4 + `/batch/${batchId}/user/${userId}`
+        endpoint: import.meta.env.VITE_PUSHER_AUTH_BASE_URL4 + `/${appUserId}/auction/${auctionUniqueId}/batch/${batchUniqueId}`
       }
     });
 
     const pusherBindEvents = async () => {
       console.log('bound');
-      const channel = pusherClient.subscribe(`presence-auction-bids-${auctionId}-batch-${batchId}`);
+      const channel = pusherClient.subscribe(`presence-auction-bids-${auctionUniqueId}-batch-${batchUniqueId}`);
       channel.trigger('client-trigger-event', { data: 'databoi1' });
 
       channel.bind("pusher:subscription_succeeded", (members) => {
@@ -41,82 +42,55 @@ function App() {
           console.log('subscription_error', data)
       });
       channel.bind("new-bid-placed", (data) => {
-        setBids(prevBids => [data, ...prevBids]) 
+        console.log('new-bid-placed');
+        setBids(prevBids => [data, ...prevBids])
       });
     };
 
     console.log('henlo');
-    getMessagesForPro()
+    getAuctionBatchBids()
     console.log(pusherClient);
     pusherBindEvents()
     return (() => {
-      pusherClient.unsubscribe(`presence-auction-bids-${auctionId}-batch-${batchId}`)
+      pusherClient.unsubscribe(`presence-auction-bids-${auctionUniqueId}-batch-${batchUniqueId}`)
     })
   }, [])
 
-  const sendMessageToPro = async () => {
-    await fetch(import.meta.env.VITE_PUSHER_BASE_URL + `/app/professionals/send_message_to_professional/` + professionalId, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${appUserAuthToken}`
-      },
-      body:JSON.stringify({ message: messageToBeSent })
-    })
-    setMessageToBeSent('')
-    console.log('sent');
-  }
-
-  // FOR PROS
-  const getMessagesForPro = async () => {
-    const response = await fetch(import.meta.env.VITE_PUSHER_BASE_URL + `/professionals/get_connection_chat_messages_as_admin/` + appUserId, {
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${proAuthToken}`
-        },
-      }
-    )
-    const data = await response.json()
-    setProMessages(data.data.chat_messages)
-  }
-
-  // FOR PROS
-  const sendMessageToAppUser = async () => {
-    await fetch(import.meta.env.VITE_PUSHER_BASE_URL + `/professionals/send_message_to_app_user/` + appUserId, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${proAuthToken}`
-      },
-      body:JSON.stringify({ message: messageToBeSent })
-    })
-    setMessageToBeSent('')
-    console.log('sent');
-  }
+    // FOR PROS
+    const getAuctionBatchBids = async () => {
+      const response = await fetch(import.meta.env.VITE_PUSHER_BASE_URL4 + `/auctions/app/view_auction_batch_bids/${auctionUniqueId}/batch/${batchUniqueId}`, {
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${appUserToken}`
+          },
+        }
+      )
+      const data = await response.json()
+      setBids(data.data.bids)
+    }
 
   return (
     <>
       <h5>{statusMessage ? statusMessage : ''}</h5>
-      <h5>{otherPersonIsOnline ? 'Other person is online' : 'Other person is offline'}</h5>
+      <h5>People online: {onlineCount}</h5>
       <div style={{ display: "flex" }}>
         <br />
-        {/* FOR APP USERS */}
+        {/* Bids */}
         <div style={{ padding: "25px", borderRightStyle: 'solid', borderRightColor: 'white', borderRightWidth: 2 }}>
           <h6> all App User Messages: </h6>
+          <hr />
           {bids.map((bid, index) => {
             return <div key={index}>
-              <p>{bid.bid_amount}</p>
-              <p>{bid.bid_amount}</p>
+              <p>id: {bid.id}</p>
+              <p>uniqueId: {bid.uniqueId}</p>
+              <p>Amount: {bid.bid_amount}</p>
+              <p>Won: {bid.won}</p>
+              <p>Created At: {bid.createdAt}</p>
+              <hr />
             </div>
           })}
         </div>
       </div>
-      <br />
-      <br />
-      {usertype === "PROFESSIONAL" && <button type="button" onClick={sendMessageToAppUser}>Send Message To User</button>}
-      <br />
-      <br />
-      {<button type="button" onClick={triggerEvent}>Trigger Event</button>}
     </>
   )
 }
