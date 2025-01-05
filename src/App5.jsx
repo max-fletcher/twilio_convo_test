7f1fbb138205b4e1595f22447ada78ab7bc75926
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2025-01-05 13:17:50 +0600

    Blog socket events done. Needs some tests and forum pending.

--- src/App5.jsx
+++ src/App5.jsx
@@ -14,6 +14,7 @@ function App() {
   const appUserAuthToken1 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InVzcl9SaWgyN0xRTTVLIiwibmFtZSI6ImZsZXRjaGVyMSIsInVzZXJuYW1lIjoiZmxldGNoZXIxIiwiZW1haWwiOm51bGwsInBob25lIjoiKzg4MDE3NjIyMTQzMTUiLCJ3aGF0c2FwcF9ubyI6bnVsbCwidmVyaWZpZWQiOnRydWUsImd1ZXN0IjpmYWxzZSwiaWF0IjoxNzM1ODA4MzAxLCJleHAiOjE3Mzg0MDAzMDF9.z13Ljw6pajyUHo_0VLKGyWsAOvV9S9yOUj7sdWG9PhQ"
   const appUserAuthToken2 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InVzcl9GWTlURmZHbUlXIiwibmFtZSI6IkFsZmkgU2hhcmluIFJpenZpIiwidXNlcm5hbWUiOiJhc3JpenZpIiwiZW1haWwiOm51bGwsInBob25lIjoiKzg4MDE0MDgwMTY4NzQiLCJ3aGF0c2FwcF9ubyI6bnVsbCwidmVyaWZpZWQiOnRydWUsImd1ZXN0Ijp0cnVlLCJpYXQiOjE3MzU4MDgyNzQsImV4cCI6MTczODQwMDI3NH0.q97T9czrcApPleAp0tyff3l63V12zMEuohP5sMp7D6c"
   const blogUniqueId = 'blog_mcXDv11Aw3'
+  const commentUniqueId = 'cmt_R44u68XXdz'
   const urlParams = new URLSearchParams(window.location.search);
   const userNo = urlParams.get('userNo'); // 'FIRST'|'SECOND'
 
@@ -55,11 +56,26 @@ function App() {
       });
       channel.bind("new-comment-submitted", (data) => {
         console.log('new-comment-submitted', data);
-        setAllComments(prevMessages => [...prevMessages, data])
+        setAllComments(prevComments => [...prevComments, data])
       });
       channel.bind("new-reply-submitted", (data) => {
         console.log('new-reply-submitted', data);
-        setAllComments(prevMessages => [...prevMessages, data])
+
+        setAllComments(prevComments => {
+          console.log(data.commentUniqueId)
+          const comment = prevComments.find((comment) => {
+            console.log('looper', comment)
+            return data.commentUniqueId === comment.uniqueId
+          })
+          console.log('found comment', comment)
+          if(comment && comment.replies && comment.replies.length)
+            comment.replies = [...comment.replies, data]
+          else
+          comment.replies = [data]
+          console.log('uploaded comment', prevComments)
+          // return [...prevComments, data]
+          return [...prevComments]
+        })
       });
     };
 
@@ -86,8 +102,7 @@ function App() {
     setAllComments(data.comments)
   }
 
-  // FOR APP USERS
-  const sendMessage = async () => {
+  const sendComment = async () => {
     const response = await fetch(import.meta.env.VITE_PUSHER_BASE_URL5 + `/app/blogs/store-comment`, {
       method: "POST",
       headers: {
@@ -96,6 +111,20 @@ function App() {
       },
 
       body:JSON.stringify({ blogId: blogUniqueId, comments: messageToBeSent })
+    })
+    setMessageToBeSent('')
+    console.log('sent');
+  }
+
+  const sendReply = async () => {
+    const response = await fetch(import.meta.env.VITE_PUSHER_BASE_URL5 + `/app/blogs/store-comment-reply`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${userNo === 'FIRST' ? appUserAuthToken1 : appUserAuthToken2 }`
+      },
+
+      body:JSON.stringify({ blogId: blogUniqueId, commentId: commentUniqueId, comments: messageToBeSent })
     })
     setMessageToBeSent('')
     console.log('sent');
@@ -137,7 +166,8 @@ function App() {
       <input type="text" onChange={(e) => setMessageToBeSent(e.target.value)} value={messageToBeSent} />
       <br />
       <br />
-      <button type="button" onClick={sendMessage}>Send Message</button>
+      <button type="button" onClick={sendComment}>Send Comment</button>
+      <button style={{ marginLeft: "10px" }} type="button" onClick={sendReply}>Send Reply</button>
     </>
   )
 }
